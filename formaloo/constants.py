5a2cfca4b1a69b10a72cbb64c2cee7c0078fed3f
Hasan Noori <xishma@gmail.com>
2021-05-26 18:52:11 +0430

    adds endpoints to get form displays

--- formaloo/constants.py
+++ formaloo/constants.py
@@ -18,7 +18,8 @@ V_1_0_CREATE_GAMIFICATION_CALCULATION_JOB = '%sgamification-calculation-jobs/' %
 
 V_1_0_FORM_LIST_CREATE_ENDPOINT = '%sforms/' % V_1_0_API_BASE
 V_1_0_FORM_ITEM_ENDPOINT = '%sforms/{}/' % V_1_0_API_BASE
-
+V_1_0_FORM_DISPLAY_ADDRESS_ENDPOINT = '%sform-displays/address/{}/' % V_1_0_API_BASE
+V_1_0_FORM_DISPLAY_SLUG_ENDPOINT = '%sform-displays/slug/{}/' % V_1_0_API_BASE
 
 # HEADERS
 APPLICATION_HEADER = 'x-api-key'
