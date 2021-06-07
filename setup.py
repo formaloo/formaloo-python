553c692808fb1197c1e366e479058485031f31f6
Hasan Noori <xishma@gmail.com>
2021-06-07 19:41:23 +0430

    update version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def reqs(*f):
 
 setuptools.setup(
     name="formaloo-cdp",
-    version="0.1.10",
+    version="0.1.11",
     author="Formaloo",
     author_email="info@formaloo.com",
     description="Official SDK to use Formaloo API",
